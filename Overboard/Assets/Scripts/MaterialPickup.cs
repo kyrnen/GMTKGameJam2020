6e0a87f1369595083420d2dbf620832509e6228b
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-07-10 18:08:50 -0500

    s

---
+++ Overboard/Assets/Scripts/MaterialPickup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MaterialPickup : MonoBehaviour
+{
+    /// <summary>
+    /// OnCollisionStay is called once per frame for every collider/rigidbody
+    /// that is touching rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionStay(Collision other)
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            MaterialPickUpable materialPickUpable = other.gameObject.GetComponent<MaterialPickUpable>();
+            if (materialPickUpable != null)
+            {
+                PlayerPrefs.SetInt(materialPickUpable.resourceType, materialPickUpable.resourceAmnt);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+
+    }
+}
