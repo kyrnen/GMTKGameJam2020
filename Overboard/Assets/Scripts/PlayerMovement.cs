79e5eb6bfe249ceda20de677d95d3af7720ec49c
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-07-10 15:16:27 -0500

    a

---
+++ Overboard/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    new Rigidbody rigidbody;
+    public float speed;
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        Vector3 moveDir = transform.position + Vector3.ClampMagnitude(Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal"), 1f) * speed * Time.fixedDeltaTime;
+        rigidbody.MovePosition(moveDir);
+    }
+}
