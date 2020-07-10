447fe49b1feb4d4c084bb7f30137935526ac888b
RedFusion Design <jan.wiechers04@gmail.com>
2020-07-10 23:40:45 +0200

    Added Plank Prototype

---
+++ Overboard/Assets/testStuffRed/PlankManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankManager : MonoBehaviour
+{
+    public float TickTime;
+    public float IncedentChance;
+    public int Cooldown = 0;
+    int TimeSinceLast = 0;
+    public int[] ProblemOptions = new int[] { };
+    float TimeStorage;
+    PlankScript[] AllPlanks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AllPlanks = this.GetComponentsInChildren<PlankScript>();
+        Debug.Log(AllPlanks.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeStorage += Time.deltaTime;
+        if(TimeStorage >= TickTime)
+        {
+            TimeStorage = 0;
+            GameTick();
+        }
+    }
+
+    void GameTick()
+    {
+        TimeSinceLast++;
+        Debug.Log("GameTick");
+        if (Random.Range(0, 100) < IncedentChance && TimeSinceLast > Cooldown)
+        {
+            int LoopIterations = 0;
+            PlankScript Target = null;
+            while(Target == null && LoopIterations < AllPlanks.Length)
+            {
+                int Index = Random.Range(0, AllPlanks.Length);
+                if (!AllPlanks[Index].IsBroken)
+                {
+                    Target = AllPlanks[Index];
+                    break;
+                }
+                LoopIterations++;
+            }
+
+            if(Target != null)
+            {
+                int Severity = Random.Range(0, 100);
+                int ProblemType = 0;
+                int StartSearch = -1;
+                for (int i = 0; i < ProblemOptions.Length; i++)
+                {
+                    if (Severity > StartSearch && Severity < ProblemOptions[i])
+                    {
+                        ProblemType = i;
+                        break;
+                    }
+                    StartSearch = ProblemOptions[i];
+                }
+                Debug.Log("Creating Incedent of Type " + ProblemType.ToString());
+                Target.Break(ProblemType);
+                TimeSinceLast = 0;
+            }
+            else
+            {
+                Debug.Log("No Avaliable Target Found");
+            }
+            
+        }
+    }
+}
---
+++ Overboard/Assets/testStuffRed/PlankScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankScript : MonoBehaviour
+{
+    public bool IsBroken;
+    public int ErrorType;
+    Color[] Colors = new Color[] {Color.red, Color.blue, Color.green };
+  
+    public bool GetStaus()
+    {
+        return IsBroken;
+    }
+
+    public void Break(int Type)
+    {
+        IsBroken = true;
+        ErrorType = Type;
+        this.GetComponent<Renderer>().material.color = Colors[Type];
+    }
+
+    public void Repair()
+    {
+        IsBroken = false;
+        ErrorType = 0;
+        this.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
