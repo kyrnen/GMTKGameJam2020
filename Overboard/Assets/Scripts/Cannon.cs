75e388bc4fe7aacc3c26253caca1cc8c94f9d532
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-07-10 16:14:09 -0500

    s

--- Overboard/Assets/Scripts/Cannon.cs
+++ Overboard/Assets/Scripts/Cannon.cs
@@ -1,15 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
     [SerializeField]
     Transform player;
-    
+    public float delay;
+    public GameObject bullet;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
     /// <summary>
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
     void FixedUpdate()
     {
         transform.LookAt(player);
+    }
+
+    IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(delay);
+        Instantiate(bullet);
+        bullet.transform.position = transform.position + transform.forward * .5f;
+        bullet.transform.LookAt(player);
+        StartCoroutine(Shoot());
     }
 }
---
+++ Overboard/Assets/Scripts/PickUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+}
