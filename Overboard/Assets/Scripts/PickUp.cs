70d397af5652a5b3703ad440d22814543af1037b
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-07-10 16:34:26 -0500

    DASFA

--- Overboard/Assets/Scripts/PickUp.cs
+++ Overboard/Assets/Scripts/PickUp.cs
@@ -2,14 +2,52 @@
 
 public class PickUp : MonoBehaviour
 {
+    [SerializeField]
+    Transform objectHolder;
+    int selectedObj;
+
     /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
     /// </summary>
-    void Update()
+    void Start()
+    {
+        SelectObject();
+    }
+
+    /// <summary>
+    /// OnCollisionStay is called once per frame for every collider/rigidbody
+    /// that is touching rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionStay(Collision other)
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            Debug.Log("f");
+            PickUpID pickUpID = other.gameObject.GetComponent<PickUpID>();
+            if (pickUpID != null)
+            {
+                selectedObj = pickUpID.id;
+                SelectObject();
+                Debug.Log("c");
+                Destroy(other.gameObject);
+            }
+        }
+    }
 
+    void SelectObject()
+    {
+        int i = 0;
+        foreach (Transform weapon in objectHolder)
+        {
+            if (i == selectedObj)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
         }
     }
 }
