e47f5f8429f658d7025c8acba447c6a587f89524
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-07-10 15:44:34 -0500

    a

---
+++ Overboard/Assets/Scripts/Cannon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        transform.LookAt(player);
+    }
+}
