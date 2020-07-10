fb0e52b7046de4a5c11fc592cfc8b492cf6f928d
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-07-10 17:04:37 -0500

    a

--- Overboard/Assets/Scripts/PlayerMovement.cs
+++ Overboard/Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,61 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     new Rigidbody rigidbody;
-    public float speed;
-    Vector3 moveDir;
+    public float speed, dashSpeed, dashDelay;
+    Vector3 moveDir, lastPos, newPos;
+    bool hasDashed = true, canDash;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() => StartCoroutine(DashDelay());
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
-    void Update() => MoveInput();
+    void Update()
+    {
+        MoveInput();
+        DashInput();
+    }
 
     void MoveInput() => moveDir = Vector3.ClampMagnitude(Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal"), 1f);
+
+    void DashInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            hasDashed = false;
+            canDash = false;
+            StartCoroutine(DashDelay());
+        }
+    }
 
     /// <summary>
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
     void FixedUpdate() => Move();
 
-    void Move() => rigidbody.MovePosition(transform.position + (moveDir * speed * Time.fixedDeltaTime));
+    void Move()
+    {
+        newPos = transform.position - lastPos;
+        rigidbody.MovePosition(transform.position + (moveDir * speed * Time.fixedDeltaTime));
+        if (!hasDashed)
+        {
+            rigidbody.MovePosition(transform.position + newPos * speed);
+            hasDashed = true;
+        }
+        lastPos = transform.position;
+    }
+
+    IEnumerator DashDelay()
+    {
+        yield return new WaitForSeconds(dashDelay);
+        canDash = true;
+    }
 }
