d87404978051ae184675b147a2c12e35b6adac18
Angela Lam <angelakam.lam@gmail.com>
2020-07-10 23:52:16 -0700

    Update Player Movement
    Created Camera in Prefab. Updated Player Movement for more versatile implementation. Implementing new Dash (currently checking but not affecting)

--- Overboard/Assets/Scripts/PlayerMovement.cs
+++ Overboard/Assets/Scripts/PlayerMovement.cs
@@ -1,62 +1,115 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /// <summary>
+    //By default these will be routed to Horizontal and Vertical.
+    //In the event that there are two players, a button should trigger a function that changes axes appropriately.
+    //For now these will be adjustable in Inspector
+    /// </summary>
     [SerializeField]
-    new Rigidbody rigidbody;
-    public float speed, dashSpeed, dashDelay;
-    Vector3 moveDir, lastPos, newPos;
-    bool hasDashed = true, canDash;
-    public string horizontalInput, verticalInput, dashInput;
+    private string horizontal = "Horizontal",
+                     vertical = "Vertical";
+    //speed cannot be manipulated by other functions this way but can be modified in Inspector for debugging purposes
+    [SerializeField]
+    private float speed = 15;
+    [SerializeField]
+    private float dashSpeed = 100;
+    //decrease dash time in game over time
+    [SerializeField]
+    private float startDashTime = 0.1f;
+    //How long dash lasts
+    private float dashTime;
+    private int direction = 0;
+
+    //using rigidbody poses issues sometimes
+    private Rigidbody rb;
+    private KeyCode dash = KeyCode.Space;
+    
+
+
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
-    void Start() => StartCoroutine(DashDelay());
-
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    void Update()
+    void Start()
     {
-        MoveInput();
-        DashInput();
+        rb = GetComponent<Rigidbody>();
+        dashTime = startDashTime;
     }
 
-    void MoveInput() => moveDir = Vector3.ClampMagnitude(Vector3.forward * Input.GetAxis(verticalInput) + Vector3.right * Input.GetAxis(horizontalInput), 1f);
 
-    void DashInput()
+    private void Update()
     {
-        if (Input.GetButtonDown(dashInput) && canDash)
+        CheckDash();
+    }
+
+    void CheckDash()
+    {
+        if (direction == 0 && Input.GetKeyDown(dash))
         {
-            hasDashed = false;
-            canDash = false;
-            StartCoroutine(DashDelay());
+            if (Input.GetAxis(horizontal) == -1)
+            {
+                direction = 1;
+            }
+            else if (Input.GetAxis(horizontal) == 1)
+            {
+                direction = 2;
+            }
+            else if (Input.GetAxis(vertical) == -1)
+            {
+                direction = 3;
+            }
+            else if (Input.GetAxis(vertical) == 1)
+            {
+                direction = 4;
+            }
+            Debug.Log("Dash direction: " + direction.ToString());
+        }
+        else
+        {
+            if (dashTime <= 0)
+            {
+                direction = 0;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+                if (direction == 1)
+                {
+                    rb.velocity = Vector3.left * dashSpeed;
+                }
+                if (direction == 2)
+                {
+                    rb.velocity = Vector3.right * dashSpeed;
+                }
+                if (direction == 3)
+                {
+                    rb.velocity = Vector3.up * dashSpeed;
+                }
+                if (direction == 4)
+                {
+                    rb.velocity = Vector3.down * dashSpeed;
+                }
+            }
         }
     }
 
     /// <summary>
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
-    void FixedUpdate() => Move();
+    void FixedUpdate()
+    {
+        Move();
+    }
 
     void Move()
     {
-        newPos = transform.position - lastPos;
-        rigidbody.MovePosition(transform.position + (moveDir * speed * Time.fixedDeltaTime));
-        if (!hasDashed)
-        {
-            rigidbody.MovePosition(transform.position + newPos * speed);
-            hasDashed = true;
-        }
-        lastPos = transform.position;
-    }
-
-    IEnumerator DashDelay()
-    {
-        yield return new WaitForSeconds(dashDelay);
-        canDash = true;
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+        rb.MovePosition(transform.position + (movement * speed * Time.fixedDeltaTime));
     }
 }
