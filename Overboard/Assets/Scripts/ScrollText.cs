2738f6c9efbb7cc82bc4da80f8c34a6fdaf9e55f
RedFusion Design <jan.wiechers04@gmail.com>
2020-07-11 19:31:18 +0200

    Scrolling Credits

---
+++ Overboard/Assets/Scripts/ScrollText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollText : MonoBehaviour
+{
+    public float Speed;
+    int StartDepth = -800;
+    int EndDepth = 2400;
+
+    void Start()
+    {
+        transform.position = Vector3.up * StartDepth;
+    }
+    void Update()
+    {
+        if(transform.position.y < EndDepth)
+        {
+            transform.position += Vector3.up * Speed * Time.deltaTime;
+        }
+    }
+}
