29c79f45960a8e36405de7fb5adf9eb6afea298d
guy196 <guy.shalev01@gmail.com>
2020-07-11 11:55:11 +0300

    Timer scene manager
    added a timer and a scene manager, to call the next level use the function LoadNextScene()

---
+++ Overboard/Assets/Scripts/timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text text;
+    private float timeleft = 100;
+    void Start()
+    {
+        text.text = timeleft.ToString("F2");
+    }
+
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+
+        text.text = timeleft.ToString("F2");
+    }
+}
