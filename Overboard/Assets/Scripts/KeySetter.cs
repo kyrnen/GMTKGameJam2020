2b9efa652e008f07991a61b58320eee74ff48942
guy196 <guy.shalev01@gmail.com>
2020-07-12 17:43:36 +0300

    Revert "some keybinds"
    This reverts commit 140e02489dff5aca5d7829b65c615de27e5984fb.

--- Overboard/Assets/Scripts/KeySetter.cs
+++ Overboard/Assets/Scripts/KeySetter.cs
@@ -1,44 +1,23 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class KeySetter : MonoBehaviour
 {
     [SerializeField]
     int keyIndex;
-    [SerializeField]
-    Text text;
-    [SerializeField]
-    new string name;
-    public bool mouseIsOver;
 
-    public void SetMouseIsOver(bool isTrue)
-    {
-        mouseIsOver = isTrue;
-    }
-
+    /// <summary>
+    /// OnGUI is called for rendering and handling GUI events.
+    /// This function can be called multiple times per frame (one call per event).
+    /// </summary>
     void OnGUI()
     {
         Event e = Event.current;
-
-        if (mouseIsOver)
-            if (e.isKey)
+        if (e.isKey)
+        {
+            if (e.keyCode != KeyCode.None)
             {
-                if (e.keyCode != KeyCode.None)
-                {
-                    Keys.SetKey(e.keyCode, keyIndex);
-                }
+                Keys.SetKey(e.keyCode, keyIndex);
             }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(Input.mousePosition, Vector3.forward);
-    }
-
-    private void Update()
-    {
-        text.text = name + Keys.GetKey(keyIndex);
+        }
     }
 }
