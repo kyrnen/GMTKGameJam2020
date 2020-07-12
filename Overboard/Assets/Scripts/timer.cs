562720e5d5671081cc5d556e97076ff88cce2f2f
guy196 <guy.shalev01@gmail.com>
2020-07-12 17:43:28 +0300

    Revert "a"
    This reverts commit 42a32ce9e1ca0e2cac096aebe1b3fe564bf429c0, reversing
    changes made to 140e02489dff5aca5d7829b65c615de27e5984fb.

---
+++ Overboard/Assets/Scripts/timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text text;
+    private float timeleft = 20;
+    public GameObject endcanvasscreen;
+    void Start()
+    {
+        text.text = timeleft.ToString("F2");
+    }
+
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+
+        text.text = timeleft.ToString("F2");
+
+        if(timeleft <= 10)
+        {
+            text.color = Color.red;
+        }
+        if(timeleft <= 0)
+        {
+            endcanvasscreen.SetActive(true);
+        }
+    }
+}
