47289e5e4ff7cd0eb3b433c9bf9177d62f2b74c7
Angela Lam <angelakam.lam@gmail.com>
2020-07-12 09:17:47 -0700

    Update Main Menu
    Adjusted Jam Jar animation. Added Quit option to the game.

--- Overboard/Assets/Scripts/MainMenu.cs
+++ Overboard/Assets/Scripts/MainMenu.cs
@@ -12,4 +12,10 @@ public class MainMenu : MonoBehaviour
         optionsMenu.SetActive(true);
         mainMenu.SetActive(false);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
 }
